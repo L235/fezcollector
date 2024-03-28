f3cb31e8edc50f5bc81f5d4a51c778a729810e1b
Firefly <firefly@serenity.uk.net>
2024-03-28 14:01:37 +0000

    allow multiple runs, summary checking

--- fez_collector.py
+++ fez_collector.py
@@ -1,4 +1,5 @@
-print("fez_collector alpha.0 initialising...")
+VERSION = "0.2"
+print(f"fez_collector {VERSION} initialising...")
 import ssl
 from re import search, compile
 from contextlib import redirect_stderr
@@ -6,6 +7,7 @@ from os import devnull, environ
 from sys import exit, exc_info
 from datetime import datetime
 from json import loads
+from time import sleep
 
 from irc.client import Reactor, ServerConnectionError
 from irc.connection import Factory
@@ -15,6 +17,9 @@ from pywikibot.comms.eventstreams import EventStreams
 from pywikibot import Site, Page
 
 TARGET = environ.get("FEZ_COLLECTOR_TARGET")
+NICKNAME = environ.get("FEZ_COLLECTOR_NICKNAME") or environ.get(
+    "FEZ_COLLECTOR_USERNAME"
+)
 USERNAME = environ.get("FEZ_COLLECTOR_USERNAME")
 PASSWORD = environ.get("FEZ_COLLECTOR_PASSWORD")
 CLOAK = environ.get("FEZ_COLLECTOR_CLOAK")
@@ -31,32 +36,10 @@ config = loads(config_page.get())
 
 PAGE_INCLUDE_PATTERN = compile(f"({'|'.join(config['pageIncludePatterns'])})")
 PAGE_EXCLUDE_PATTERN = compile(f"({'|'.join(config['pageExcludePatterns'])})")
+SUMMARY_INCLUDE_PATTERN = compile(f"({'|'.join(config['summaryIncludePatterns'])})")
+SUMMARY_EXCLUDE_PATTERN = compile(f"({'|'.join(config['summaryExcludePatterns'])})")
 USER_EXCLUDE_LIST = config["userExcludeList"]
 USER_INCLUDE_LIST = config["userIncludeList"]
-
-# EventStreams setup
-stream = EventStreams(
-    streams=["recentchange", "revision-create"], since=datetime.now().isoformat()
-)
-stream.register_filter(server_name="en.wikipedia.org")
-
-# IRC setup
-ssl_factory = Factory(wrapper=ssl.wrap_socket)
-reactor = Reactor()
-try:
-    irc_c = reactor.server().connect(
-        "irc.libera.chat",
-        6697,
-        USERNAME,
-        username=USERNAME,
-        password=PASSWORD,
-        connect_factory=ssl_factory,
-    )
-except ServerConnectionError as exc:
-    print(exc_info()[1])
-    raise exit(1) from exc
-
-irc_c.join(TARGET)
 
 
 def format_message(_change):
@@ -84,8 +67,6 @@ def command_handler(c, e):
             ),
         )
         c.disconnect()
-        print("Failsafe command used - exiting.")
-        exit(1)
     if msg == "!ping":
         c.privmsg(TARGET, colored("pong", "Pink", padding=""))
 
@@ -95,24 +76,91 @@ def join_handler(c, e):
     target = e.target
     if USERNAME in source and CLOAK not in source:
         print(CLOAK_ERROR_MSG)
-        c.quit(CLOAK_ERROR_MSG)
-        exit(1)
+        c.disconnect(CLOAK_ERROR_MSG)
     print(f"Joined {target}!")
 
 
+def nick_handler(c, e):
+    print("Nickname in use, changing...")
+    if NICKNAME != USERNAME:
+        c.nick(NICKNAME)
+    else:
+        c.nick(f"{USERNAME}_")
+
+
+def connect_handler(c, e):
+    irc_c.join(TARGET)
+
+
+def disconnect_handler(c, e):
+    print("IRC disconnect - exiting...")
+    exit(1)
+
+
+# EventStreams setup
+stream = EventStreams(
+    streams=["recentchange", "revision-create"], since=datetime.now().isoformat()
+)
+stream.register_filter(server_name="en.wikipedia.org")
+
+# IRC setup
+ssl_factory = Factory(wrapper=ssl.wrap_socket)
+reactor = Reactor()
+try:
+    irc_c = reactor.server().connect(
+        "irc.libera.chat",
+        6697,
+        USERNAME,
+        username=USERNAME,
+        password=PASSWORD,
+        connect_factory=ssl_factory,
+        sasl_login=USERNAME,
+    )
+except ServerConnectionError as exc:
+    print(exc_info()[1])
+    raise exit(1) from exc
+
+
+irc_c.add_global_handler("welcome", connect_handler)
+irc_c.add_global_handler("nicknameinuse", nick_handler)
 irc_c.add_global_handler("pubmsg", command_handler)
 irc_c.add_global_handler("join", join_handler)
+irc_c.add_global_handler("disconnect", disconnect_handler)
+
+reactor.process_once()
 print("initialised!")
 
-# We do this as the EventStreams API dumps a metric crapload of 'errors' to stderr
-with redirect_stderr(open(devnull, "w", encoding="utf-8")):
-    for change in iter(stream):
-        title = change["title"]
-        user = change["user"]
-        if (
-            search(PAGE_INCLUDE_PATTERN, title)
-            and not search(PAGE_EXCLUDE_PATTERN, title)
-            and user not in USER_EXCLUDE_LIST
-        ) or user in USER_INCLUDE_LIST:
-            irc_c.privmsg(TARGET, format_message(change))
-        reactor.process_once()
+try:
+    # We do this as the EventStreams API dumps a metric crapload of 'errors' to stderr
+    with redirect_stderr(open(devnull, "w", encoding="utf-8")):
+        for change in iter(stream):
+            title = change["title"]
+            user = change["user"]
+            comment = (
+                change["log_action_comment"]
+                if "log_action_comment" in change
+                else change["comment"]
+            )
+
+            # Exclude rules - if any of these match, we're done
+            if (
+                user in USER_EXCLUDE_LIST
+                or search(PAGE_EXCLUDE_PATTERN, title)
+                or search(SUMMARY_EXCLUDE_PATTERN, comment)
+            ):
+                continue
+
+            # Include rules - if any of these match, we post
+            if (
+                user in USER_INCLUDE_LIST
+                or search(PAGE_INCLUDE_PATTERN, title)
+                or search(SUMMARY_INCLUDE_PATTERN, comment)
+            ):
+                irc_c.privmsg(TARGET, format_message(change))
+
+            reactor.process_once()
+# Done to ensure we exit cleanly and the continuous job (on Toolforge) gets restarted
+except Exception as err:
+    irc_c.disconnect()
+    print(err)
+    exit(1)
